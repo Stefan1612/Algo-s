16eeb8acf0ffb9991477c0894f88288a214c4d0d
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-10-11 18:14:03 +0200

    Update Stack.js

--- Data-Structures/Stack.js
+++ Data-Structures/Stack.js
@@ -1,1 +1,64 @@
+// -----------------------------------
+// High level version of stack using arrays
+// -----------------------------------
 
+class Stack {
+  constructor() {
+    this.count = 0;
+    this.storage = [];
+  }
+
+  push(value) {
+    this.storage[this.count] = value;
+    console.log(`${value} added to ${this.count}`);
+    this.count++;
+  }
+
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    }
+    console.log(`${this.storage[this.count - 1]} removed from ${this.count}`);
+    this.count--;
+    let result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
+
+  size() {
+    console.log(this.count);
+    return this.count;
+  }
+
+  peek() {
+    console.log(this.storage[this.count - 1]);
+    return this.storage[this.count - 1];
+  }
+
+  isEmpty() {
+    console.log(this.count === 0);
+    return this.count === 0;
+  }
+}
+
+const stack = new Stack();
+
+stack.isEmpty();
+
+stack.push(100);
+stack.push(200);
+stack.peek();
+stack.size();
+
+stack.pop();
+stack.size();
+
+stack.push(400);
+stack.isEmpty();
+
+stack.peek();
+stack.pop();
+
+// -----------------------------------
+// High level version of stack using arrays
+// -----------------------------------
