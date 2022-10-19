f151dafc654950b77ee0606c6f1d9cea8b03c7d2
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-10-19 02:27:51 +0200

    Update Merge-Sort.js

--- Algorithms/Merge-Sort.js
+++ Algorithms/Merge-Sort.js
@@ -1,1 +1,43 @@
+const merge = (leftArr, rightArr) => {
+  const output = []
+  let leftIndex = 0;
+  let rightIndex = 0;
+  
+  while(leftIndex < leftArr.length && rightIndex < rightArr.length){
+      const leftEl = leftArr[leftIndex]
+      const rightEl  = rightArr[rightIndex]
+      
+      if(leftEl < rightEl){
+          output.push(leftEl) 
+        leftIndex++
+      } else {
+          output.push(rightEl) 
+        rightIndex++
+      }
+  }
+  
+  return [...output, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)]
+}
 
+
+
+const mergeSort = (array) => {
+  
+  if(array.length <= 1}{
+    return array
+  }
+  
+  const middleIndex = Math.floor(array.length / 2)
+  
+  const leftArr = array.slice(0, middleIndex)
+  const rightArr = array.slice(middleIndex)
+  
+  return merge(
+    mergeSort(leftArr), 
+    mergeSort(rightArr)
+  )
+  
+}
+
+
+// https://www.youtube.com/watch?v=x_Z9FcAPmbk
