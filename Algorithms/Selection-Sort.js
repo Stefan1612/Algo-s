7472c8775e044e40dd22a471c8cb44c49739ce23
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-10-20 21:28:08 +0200

    Update Selection-Sort.js

--- Algorithms/Selection-Sort.js
+++ Algorithms/Selection-Sort.js
@@ -1,1 +1,27 @@
 
+function selectionSort(array){
+  
+  let toSort = array
+  
+  let sortedArray = []
+    
+    while(sortedArray.length !== array.length){
+      
+      let highestNum = toSort[0]
+      
+      for(let i = 0; i < toSort.length; i++){
+        
+        if(toSort[i] > highestNum){
+            highestNum = toSort[i]
+            toSort[i] = toSort[0]
+            toSort[0] = highestNum
+        }
+        
+      }
+      sortedArray.push(highestNum)
+      toSort.shift()
+    }
+  
+
+  return sortedArray
+}
