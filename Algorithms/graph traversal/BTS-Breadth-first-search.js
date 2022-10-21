642827cc5595da7d410c475fd72479305794a156
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-10-21 05:07:12 +0200

    Update BTS-Breadth-first-search.js

--- Algorithms/graph traversal/BTS-Breadth-first-search.js
+++ Algorithms/graph traversal/BTS-Breadth-first-search.js
@@ -1,1 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
 
+var getTargetCopy = function(original, cloned, target) {
+    
+    let queue = [[original, cloned]];
+    
+    while(queue.length) {
+        const next = [];
+        
+        for(let [oNode, cNode] of queue) {
+            if(oNode === target) {
+                return cNode;
+            }
+            if(oNode.left) next.push([oNode.left, cNode.left]);
+            if(oNode.right) next.push([oNode.right, cNode.right]);
+        }
+        queue = next;
+    }
+};
+
+// Input: tree = [7,4,3,null,null,6,19], [7,4,3,null,null,6,19],  3
+// Output: 3
