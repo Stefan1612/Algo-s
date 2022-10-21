bf6725c765abe9d22f400eaa81c12deeb6e1aae6
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-10-21 13:13:48 +0200

    Update LinkedList.js

--- Data-Structures/LinkedList.js
+++ Data-Structures/LinkedList.js
@@ -1,1 +1,19 @@
 
+class Node {
+ constructor(data, next, null){
+    this.data = data;
+    this.next = next
+ }
+}
+
+class LinkedList{
+  constructor(){
+    this.head = null
+    this.size = 0;
+  }
+  
+  insertFirst() {
+    this.head = new Node(data, this.head)
+  }
+  
+}
