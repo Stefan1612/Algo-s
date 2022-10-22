7e6ebd2b62ccaffd678ca2e0a1af7bae5b2cb2cf
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-10-22 21:52:20 +0200

    Update Insertion-Sort.js

--- Algorithms/Insertion-Sort.js
+++ Algorithms/Insertion-Sort.js
@@ -1,1 +1,22 @@
 
+function insertionSort(){
+  
+  const arr = array.slice()
+  for(let i = 1;  i < arr.length; i++){
+    
+    for(let j = i; j > 0; j--){
+      
+      
+      if(arr[j] < arr[j - 1) {
+        [arr[j], arr[j - 1]] = [arr[j-1], arr[j]]
+      }
+      else {
+        break;
+      }
+    }
+  }
+  
+  return arr
+  
+  
+}
