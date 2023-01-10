608c69c0e230323678bec6e1000aff89d958a706
Stefan Lehmann <bernd49634@web.de>
2023-01-10 19:35:41 +0100

    added selection - and bubble sort

--- Algorithms/Bubble-Sort.js
+++ Algorithms/Bubble-Sort.js
@@ -33,3 +33,39 @@ function bubbleSort(unsortedArray) {
 }
 
 console.log(bubbleSort([1, 31, 312, 12, 31, 3, 23]));
+
+/**
+ *
+ * optimize bubble sort
+ */
+
+function bubbleSortOptimized(unsortedArray) {
+  if (unsortedArray.length === 0) {
+    return undefined;
+  }
+  for (let i = unsortedArray.length - 1; i >= 0; i--) {
+    let counter = 0;
+    for (let j = 0; j < i; j++) {
+      console.log(`${unsortedArray[j]} vs ${unsortedArray[j + 1]}`);
+      if (unsortedArray[j] > unsortedArray[j + 1]) {
+        let temp = unsortedArray[j];
+        unsortedArray[j] = unsortedArray[j + 1];
+        unsortedArray[j + 1] = temp;
+        counter++;
+      }
+    }
+    if (counter === 0) {
+      break;
+    }
+  }
+  return unsortedArray;
+}
+
+console.log(bubbleSortOptimized([1, 31, 312, 12, 31, 3, 23]));
+console.log(bubbleSortOptimized([1, 2, 11, 15, 14]));
+
+/**
+ * runtime : o(n^2) (worst case)
+ * space: o(n^2)
+ *
+ */
--- Algorithms/Selection-Sort.js
+++ Algorithms/Selection-Sort.js
@@ -1,29 +1,49 @@
-
-function selectionSort(array){
-  
-  let toSort = array
-  
-  let sortedArray = []
-    
-    while(sortedArray.length !== array.length){
-      
-      let highestNum = toSort[0]
-      
-      for(let i = 0; i < toSort.length; i++){
-        
-        if(toSort[i] > highestNum){
-            highestNum = toSort[i]
-            toSort[i] = toSort[0]
-            toSort[0] = highestNum
-        }
-        
+function selectionSort(array) {
+  let temp = 0;
+  let highest = array[0];
+  for (let i = 0; i < array.length; i++) {
+    highest = i;
+    for (let j = i; j < array.length; j++) {
+      if (array[highest] < array[j]) {
+        highest = j;
       }
-      sortedArray.push(highestNum)
-      toSort.shift()
     }
-  
+    temp = array[i];
+    array[i] = array[highest];
+    array[highest] = temp;
+  }
 
-  return sortedArray
+  return array;
+}
+
+// this function is not pure
+
+function goodSelectionSort(unsortedArray) {
+  //  we NEED to use slice() or ELSE WE WILL MODIFY THE ORIGINAL ARRAY WITH EACH FIRST LOOP ITERATION (splice()), and therefore reduce our loop counter even tho need it to stay the same
+  let toSort = unsortedArray.slice();
+  let res = [];
+
+  let highest = unsortedArray[0];
+  let index = 0;
+
+  for (let i = 0; i < unsortedArray.length; i++) {
+    for (let j = toSort.length - 1; j >= 0; j--) {
+      if (toSort[j] > highest) {
+        highest = toSort[j];
+        index = j;
+      }
+    }
+    res.push(highest);
+    toSort.splice(index, 1);
+
+    highest = toSort[0];
+  }
+
+  return res;
 }
 
 //this is a pure function because it doesn't mutate the original array
+console.log(goodSelectionSort([1, 31, 312, 12, 31, 3, 23]));
+console.log(goodSelectionSort([1, 2, 11, 15, 14]));
+console.log(selectionSort([1, 31, 312, 12, 31, 3, 23]));
+console.log(selectionSort([1, 2, 11, 15, 14]));
